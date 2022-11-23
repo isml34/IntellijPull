c7ceff5230c42e79c598ff7db2cb6da0dd9a4de7
isml34 <117315848+isml34@users.noreply.github.com>
2022-11-23 20:58:10 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helo world");
+        System.out.println("Hello world");
+        //githup ucuncu gud
     }
 }
